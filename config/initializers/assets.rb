87505f54ff2a16951ee8bf34d34d8f24ed8019cc
joshneipp <joshneipp@gmail.com>
2015-09-26 11:19:01 -0400

    Add yelp client to assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = ENV['YELP_CONSUMER_KEY']
+  config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
+  config.token = ENV['YELP_TOKEN']
+  config.token_secret = ENV['YELP_TOKEN_SECRET']
+end
